85657f335afe1c56c80755bb09ad1c127b98563c
Ojigbo Leo <ojigboleo@gmail.com>
2019-05-29 11:36:13 +0300

    Complete front page

---
+++ what-i-do/src/components/Display/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Footer.css'
+
+export class Footer extends Component {
+  render() {
+    return (
+      <div className='footer'>
+        <p>&copy; 2019 Leo Ojigbo.</p>
+      </div>
+    )
+  }
+}
+
+export default Footer
