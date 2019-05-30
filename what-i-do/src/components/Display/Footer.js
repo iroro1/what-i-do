bfae228d122c113cd8375ecdb5e61d93bfe53e1c
Ojigbo Leo <ojigboleo@gmail.com>
2019-05-30 11:54:39 +0300

    add target attribute for social media  links

--- what-i-do/src/components/Display/Footer.js
+++ what-i-do/src/components/Display/Footer.js
@@ -14,9 +14,9 @@ export class Footer extends Component {
           <p><i class="fas fa-phone mx-2 text-warning"></i><a className='text-warning' href='tel:+905338572105'>+905338572105</a></p>
         <div>
          <a className='text-warning p-2' href='mailto:ojigboleo@gmail.com'><i class="fas fa-envelope fa-2x"></i></a>
-         <a className='text-danger p-2' href='https://www.instagram.com/iroro1/'><i class="fab fa-instagram fa-2x"></i></a>
-         <a className='text-primary p-2' href='https://www.linkedin.com/in/iroro1/'> <i class="fab fa-linkedin fa-2x"></i></a>
-          <a className='text-light p-2' href='https://github.com/iroro1'><i class="fab fa-github fa-2x"></i></a>
+         <a className='text-danger p-2' href='https://www.instagram.com/iroro1/' target='_blank'><i class="fab fa-instagram fa-2x"></i></a>
+         <a className='text-primary p-2' href='https://www.linkedin.com/in/iroro1/' target='_blank'> <i class="fab fa-linkedin fa-2x"></i></a>
+          <a className='text-light p-2' href='https://github.com/iroro1' target='_blank'><i class="fab fa-github fa-2x"></i></a>
         </div> 
         </div>
         
