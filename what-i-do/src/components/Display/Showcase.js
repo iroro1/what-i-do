e3f8acc7106957566dd9f51dae46850c33db5ff7
Ojigbo Leo <ojigboleo@gmail.com>
2019-06-06 13:11:49 +0300

    patron correction

--- what-i-do/src/components/Display/Showcase.js
+++ what-i-do/src/components/Display/Showcase.js
@@ -26,7 +26,7 @@ class Showcase extends Component {
                 <p>So irrespective of profession, get on now and show what you do. Its wont't cost you a dime, from hair dressing, baking , photography, cooking etc or you just want to advertise yourself as available to work.</p>
                 <p className='lead'>For enquires or complaints hit the button below.</p>
                 <a href='mailto:ojigboleo@gmail.com' className='btn btn-outline-warning'>Send a Mail</a>
-                <p className='mt-2'><b > Help keep this app free. Become a Patr   on with a minimum of 1 dollar or more a month!!!</b></p>
+                <p className='mt-2'><b > Help keep this app free. Become a Patron with a minimum of 1 dollar or more a month!!!</b></p>
                 <a className='btn btn-success' href="https://www.patreon.com/bePatron?u=20675470" data-patreon-widget-type="become-patron-button" target='_blank' rel="noopener noreferrer">Become a Patron!</a><script async src="https://c6.patreon.com/becomePatronButton.bundle.js"></script>
             <div style={border} >
                 <span  className='text-danger container'>*
